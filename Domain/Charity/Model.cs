15291e07b40e1ef469451be966d68ba4fe135c2a
yekkhan <yekkhan@atomi.cloud>
2025-08-10 15:52:29 +0800

    feat: update charity domain model

--- Domain/Charity/Model.cs
+++ Domain/Charity/Model.cs
@@ -1,9 +1,20 @@
 namespace Domain.Charity
 {
-    public class CharityModel
+    public class CharityRecord
     {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Email { get; set; }
+      public required string Name { get; set; }
+      public required string Email { get; set; }
+      public required string Address { get; set; }
+    }
+    
+    public class CharityPrincipal
+    {
+      public required Guid Id { get; set; }
+      public required CharityRecord Record { get; set; }
+    }
+
+    public class Charity
+    {
+      public required CharityPrincipal Principal { get; set; }
     }
 }
