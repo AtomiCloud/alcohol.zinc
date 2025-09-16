9176da9ab61c4d9db0a190dac9d4945d34882194
yekkhan <yekkhan@atomi.cloud>
2025-09-16 22:34:14 +0800

    feat: habit model migration

--- App/Migrations/20250916101321_UpdateHabitStructureRemoveDatesAddEnabled.Designer.cs
+++ App/Migrations/20250916101321_UpdateHabitStructureRemoveDatesAddEnabled.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using App.StartUp.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace App.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250916101321_UpdateHabitStructureRemoveDatesAddEnabled")]
+    partial class UpdateHabitStructureRemoveDatesAddEnabled
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
