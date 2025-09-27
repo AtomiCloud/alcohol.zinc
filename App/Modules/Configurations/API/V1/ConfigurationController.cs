f6957894b42350ad1ea140064ee5da9b6cd865bd
yekkhan <yekkhan@atomi.cloud>
2025-09-27 17:23:45 +0800

    refactor: refactor configuration controller

--- App/Modules/Configurations/API/V1/ConfigurationController.cs
+++ App/Modules/Configurations/API/V1/ConfigurationController.cs
@@ -35,8 +35,8 @@ public class ConfigurationController(
 
     // Admin can access any configuration, regular users only their own
     var result = h.HasAny(HttpContext.User, AuthRoles.Field, AuthRoles.Admin) 
-      ? await service.GetById(id).Then(config => config?.ToRes(), Errors.MapAll)
-      : await service.GetById(id, userId).Then(config => config?.ToRes(), Errors.MapAll);
+      ? await service.GetById(id).Then(config => config?.ToRes(), Errors.MapNone)
+      : await service.GetById(id, userId).Then(config => config?.ToRes(), Errors.MapNone);
       
     return this.ReturnNullableResult(result, new EntityNotFound("Configuration Not Found", typeof(Configuration), id.ToString()));
   }
@@ -54,7 +54,7 @@ public class ConfigurationController(
     var result = await createConfigurationReqValidator
       .ValidateAsyncResult(req, "Invalid CreateConfigurationReq")
       .ThenAwait(r => service.Create(userId, r.ToRecord()))
-      .Then(config => config.ToRes().ToResult());
+      .Then(config => config.ToRes(), Errors.MapNone);
     return this.ReturnResult(result);
   }
 
@@ -71,7 +71,7 @@ public class ConfigurationController(
     var result = await updateConfigurationReqValidator
       .ValidateAsyncResult(req, "Invalid UpdateConfigurationReq")
       .ThenAwait(r => service.Update(id, userId, r.ToRecord()))
-      .Then(config => (config?.ToRes()).ToResult());
+      .Then(config => (config?.ToRes()), Errors.MapNone);
     return this.ReturnNullableResult(result, new EntityNotFound("Configuration Not Found", typeof(ConfigurationPrincipal), id.ToString()));
   }
 
