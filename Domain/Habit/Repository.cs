b1e255ff4ef6a0350073d8b85e366ec55146ffbe
yekkhan <yekkhan@atomi.cloud>
2025-10-04 16:42:30 +0800

    fix: resolve conflict

--- Domain/Habit/Repository.cs
+++ Domain/Habit/Repository.cs
@@ -17,8 +17,8 @@ namespace Domain.Habit
         Task<Result<int>> CreateFailedExecutions(List<string> userIds, DateOnly date);        // Batch create failed executions
 
         // Habit Execution Methods
-        Task<Result<DateOnly>> GetUserCurrentDate(string userId);                    // Get current date in user's timezone
-        Task<Result<HabitExecutionPrincipal>> CompleteHabit(string userId, Guid habitId, DateOnly date, string? notes);
+        Task<Result<DateOnly>> GetUserCurrentDate(string userId, Guid habitVersionId);                    // Get current date in user's timezone
+        Task<Result<HabitExecutionPrincipal>> CompleteHabit(string userId, Guid habitVersionId, DateOnly date, string? notes);
         Task<Result<List<HabitExecutionPrincipal>>> SearchHabitExecutions(string userId, 
           HabitExecutionSearch habitExecutionSearch);
     }
